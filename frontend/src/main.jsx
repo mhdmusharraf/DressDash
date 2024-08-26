fc38ddf3a90af647701298f038f582df79979fd0
mhdmusharraf <mhdmusharraf.edu@gmail.com>
2024-08-26 18:26:37 +0530

    creating navigation bar

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById("root")).render(
+ReactDom.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
