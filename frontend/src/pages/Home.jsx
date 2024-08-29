e79bf0eff21e75c5d8da8ea97e050479057b5595
mhdmusharraf <mhdmusharraf.edu@gmail.com>
2024-08-29 23:14:51 +0530

    Working on latest product section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import Hero from '../components/Hero'
+import React from "react";
+import Hero from "../components/Hero";
+import LatestCollection from "../components/LatestCollection";
 
 const Home = () => {
   return (
     <div>
-      <Hero/>
+      <Hero />
+      <LatestCollection />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
