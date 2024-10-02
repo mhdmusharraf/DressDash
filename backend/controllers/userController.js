5efae8a6838bfbcbea9e4f8e297cc3a80ba5a7b8
mhdmusharraf <mhdmusharraf.edu@gmail.com>
2024-10-02 16:21:18 +0530

    loginUser controller part done

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -8,7 +8,27 @@ const createToken = (id) => {
 };
 
 // Route for user login
-const loginUser = async (req, res) => {};
+const loginUser = async (req, res) => {
+  try{
+    const {email,password}=req.body;
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: "User doesn't exists" });
+    }
+    const isMatch = await bcrypt.compare(password,user.password);
+    if(isMatch){
+      const token = createToken(user._id);
+      res.json({success:true,token})
+    }
+    else{
+      return res.status(401).json({success:false, message: "Invalid credentials" });
+    }
+
+  }catch(error){
+    console.log(error)
+    res.status(500).json({ message: "Error logging in user" });
+  }
+};
 
 // Route for user register
 const registerUser = async (req, res) => {
