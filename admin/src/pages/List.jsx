f462741eb8cb3e6284af22c48a11f01e7928a9f5
mhdmusharraf <mhdmusharraf.edu@gmail.com>
2024-10-16 11:20:58 +0530

    List part done

--- admin/src/pages/List.jsx
+++ admin/src/pages/List.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 import { backendUrl, currency } from "../App";
 import { toast } from "react-toastify";
 
-const List = () => {
+const List = ({ token }) => {
   const [list, setList] = useState([]);
 
   const fetchList = async () => {
@@ -16,6 +16,25 @@ const List = () => {
       }
     } catch (error) {
       console.error(error);
+      toast.error(error.message);
+    }
+  };
+
+  const removeProduct = async (id) => {
+    try {
+      const response = await axios.post(
+        backendUrl + "/api/product/remove",
+        { id },
+        { headers: { token } }
+      );
+      if (response.data.success) {
+        toast.success(response.data.message);
+        await fetchList();
+      } else {
+        toast.error(response.data.message);
+      }
+    } catch (error) {
+      console.log(error);
       toast.error(error.message);
     }
   };
@@ -36,17 +55,26 @@ const List = () => {
           <b className="text-center">Action</b>
         </div>
         {/* ---------- Product List ---------- */}
-        {
-          list.map((item,index)=>(
-            <div className="grid grid-cols-[1fr_3fr_1fr] md:grid-cols-[1fr_3fr_1fr_1fr_1fr] items-center gap-2 py-1 px-2 border text-sm" key={index}>
-              <img className="w-12" src={item.image[0]} alt="" />
-              <p>{item.name}</p>
-              <p>{item.category}</p>
-              <p>{currency}{item.price}</p>
-              <p className="text-right md:text-center cursor-pointer text-lg">X</p>
-            </div>
-          ))
-        }
+        {list.map((item, index) => (
+          <div
+            className="grid grid-cols-[1fr_3fr_1fr] md:grid-cols-[1fr_3fr_1fr_1fr_1fr] items-center gap-2 py-1 px-2 border text-sm"
+            key={index}
+          >
+            <img className="w-12" src={item.image[0]} alt="" />
+            <p>{item.name}</p>
+            <p>{item.category}</p>
+            <p>
+              {currency}
+              {item.price}
+            </p>
+            <p
+              onClick={() => removeProduct(item._id)}
+              className="text-right md:text-center cursor-pointer text-lg"
+            >
+              X
+            </p>
+          </div>
+        ))}
       </div>
     </>
   );
