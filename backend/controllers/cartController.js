97999f2019d22ea964d2a6b016ea1c0c021ce672
mhdmusharraf <mhdmusharraf.edu@gmail.com>
2024-11-05 23:58:57 +0530

    Addtocart function

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -1,5 +1,27 @@
 // add products to user cart
-const addToCart = async (req, res) => {};
+const addToCart = async (req, res) => {
+    try {
+        const {userId,itemId,size}=req.body;
+        const userData = await userModel.findById(userId);
+        let cartData = await userData.cartData;
+        if(cartData[itemId]){
+            if(cartData[itemId][size]){
+                cartData[itemId][size]+=1;
+            }
+            else{
+                cartData[itemId][size]=1;
+            }
+        }else{
+            cartData[itemId]={};
+            cartData[itemId][size]=1;
+        }
+        await userModel.findByIdAndUpdate(userId,{cartData})
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+
+        
+    }
+};
 
 // update user cart
 const updateCart = async (req, res) => {};
